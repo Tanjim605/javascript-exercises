f847e03c143ef53e79c0215d3afd7329d9a9b475
Tanjim605 <tanjimulislam2001@gmail.com>
2025-02-14 18:39:13 +0600

    jest till _04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    // eikhane jodi empty string set na kori taile undefined set thake shurute
+    let result = "";
+    // console.log(string.length -1);
+    for (let i = string.length-1; i >= 0; i--) {
+        result += string[i];
+    }
+    // console.log(result);
+    return result;
+    
 };
+
+// reverseString("1234")
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+    // const arg = [...arguments]; // joto arguments pass hoise oder shobaike ekta array te convert kore dey
+
+    // console.log(arg);
+    // console.log(num);
+
+    const dels = num;
+    // console.log("dels: ", dels);
+
+    for (let i = 0; i < array.length; i++) {
+
+        // let x = dels.find((el) => { el == array[i] })        // eitate curly bracket thakay kaj kore nai
+        let x = dels.find((el) => el === array[i])
+        // console.log(x, array[i]);
+
+        if (x) {     // if found in x then remove it
+            for (let j = i; j < array.length; j++) {
+                array[j] = array[j + 1];
+            }
+            array.pop();
+            i = -1;     // -1 e jawar reason hoilo, loop er kaj e gele abar ++ hoye 0 theke shuru hoy
+        }
+    }
+    console.log(array);
+
+    return array;
 };
+
+removeFromArray([1, 2, 3], "1", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
