1cf26cf82dbae48f9068205654146c66ec96d34f
Tanjim605 <tanjimulislam2001@gmail.com>
2025-02-16 21:37:44 +0600

    07 passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = (5 / 9) * (fahrenheit - 32);
+  // celsius = celsius.toFixed(2);  // gives 0.00 for 0;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  // fahrenheit = fahrenheit.toFixed(2);
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
